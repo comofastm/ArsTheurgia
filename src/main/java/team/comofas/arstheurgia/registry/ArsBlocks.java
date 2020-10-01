c5274ce437d595d0faae60473e2aeb160bbba741
KaueTheTrekker <kauegames9@gmail.com>
2020-10-01 03:47:00 -0300

    Optimized some code, added ArsBlocks and blocks to ArsUtils, started implementation on the ritual systems

---
+++ src/main/java/team/comofas/arstheurgia/registry/ArsBlocks.java
@@ -0,0 +1,22 @@
+package team.comofas.arstheurgia.registry;
+
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.Item;
+import net.minecraft.util.registry.Registry;
+import team.comofas.arstheurgia.blocks.RitualBlock;
+import team.comofas.arstheurgia.ArsUtils;
+
+
+public class ArsBlocks {
+
+    public static final Block RITUALCENTER = new RitualBlock(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+
+    public static void registerAll() {
+        Registry.register(Registry.BLOCK, ArsUtils.getIdentifier("ritual_block"), RITUALCENTER);
+    }
+    public static Item registerBlock(Item item, String name) {
+        return Registry.register(Registry.ITEM, ArsUtils.getIdentifier(name), item);
+    }
+}
