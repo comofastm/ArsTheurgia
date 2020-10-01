1e6500cc9a9486026a1ba59998ecadc0a5f4be29
cominixo01 <22600756+cominixo01@users.noreply.github.com>
2020-09-30 21:28:30 -0300

    Initial commit.

---
+++ src/main/java/team/comofas/arstheurgia/ArsTheurgia.java
@@ -0,0 +1,10 @@
+package team.comofas.arstheurgia;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ArsTheurgia implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
